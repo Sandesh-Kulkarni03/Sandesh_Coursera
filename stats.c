33c94f4b10ac03312c187ac0402e9829fa2eae81
Sandesh Kulkarni <sandeshrvkulkarni@gmail.com>
2023-11-05 09:38:20 -0700

    Submitting the complete code for the C1M1 assessment

--- stats.c
+++ stats.c
@@ -9,35 +9,165 @@
  *
  *****************************************************************************/
 /**
- * @file Kulkarni_Coursera 
- * @brief <Add Brief Description Here >
+ * File : Kulkarni_Coursera 
+ * Brief :  This programme aims to create a simple application,
+ * that performs statistical analysis on a dataset.
  *
- * <Add Extended Description Here>
+ * 
  *
- * @author Sandesh Kulkarni
- * @date 31-10-2023 
+ * Author : Sandesh Kulkarni
+ * Date :  05-11-2023 
  * *
  */
 
 
 
 #include <stdio.h>
+#include <stdlib.h>
+//Header file with function prototype
 #include "stats.h"
 
-/* Size of the Data Set */
-#define SIZE (40)
 
-void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-
+// Function to swap two elements in an array
+void swap(unsigned char *a, unsigned char *b)
+{
+     unsigned char temp = *a;
+      *a = *b;
+      *b = temp;
 }
 
-/* Add other Implementation File Code Here */
+// Function to sort an array in descending order using the bubble sort algorithm
+void sort_array(unsigned char arr[], int size)
+{
+       	for (int i = zero; i < size -one; i++)
+	{
+		for (int j = zero; j < size - i - one; j++) 
+		{
+			if (arr[j] < arr[j + one]) 
+			{
+				 swap(&arr[j], &arr[j + one]);
+			}
+		}
+	}
+}
+
+// Function to find the maximum value in the array
+unsigned char find_maximum(const unsigned char *data, int size) 
+{
+     unsigned char max = data[zero];
+     for (int i = one; i < size; i++)
+     {
+     	 if (data[i] > max)
+	 {
+         	max = data[i];
+         }
+     }
+     return max;
+}
+
+// Function to find the minimum value in the array
+unsigned char find_minimum(const unsigned char *data, int size)
+{
+     unsigned char min = data[zero];
+     for (int i = one; i < size; i++)
+     {
+     	if (data[i] < min) 
+	{
+        	min = data[i];
+        }
+     }
+     return min;
+}
+
+// Function to find the mean of the array
+int find_mean(const unsigned char *data, int size)
+{
+     int sum = zero;
+     for (int i = zero; i < size; i++)
+     {
+	 sum += data[i];
+     }
+	return sum / size;
+}
+
+// Function to find the median of the array
+int find_median(unsigned char arr[], const unsigned char *data, int size) 
+{
+       	sort_array(arr, size);
+
+	if (size % 2 == zero)
+       	{
+        	int mid1 = arr[size / 2 - one];
+		int mid2 = arr[size / 2];
+		return (mid1 + mid2) / 2;
+	}
+       	else
+       	{
+		return arr[size / 2];
+        }
+}
+
+// Print the results
+int print_statistics(unsigned char arr[], unsigned char *data, int size)
+{
+	//Find the maximum, minimum, mean and median
+	unsigned char max = find_maximum(data, size);
+	unsigned char min = find_minimum(data, size);
+	int mean = find_mean(data, size);
+	int median = find_median(data, data, size);
+	
+	printf("Maximum: %d\n", max);
+ 	printf("Minimum: %d\n", min);
+ 	printf("Mean: %d\n", mean);
+ 	printf("Median: %d\n", median);
+}  
+
+//Print original unsorted array
+void print_array(const unsigned char *data, int size)
+{	
+	printf("Unsorted Data :\n");
+	for(int i = zero ; i < size ; i++)
+	{
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+} 
+
+
+int main() 
+{
+	int size;
+	        
+	// Input: You can change this array to your data
+	unsigned char data[] = {34,201,190,154,8,194,2,6,
+		  		  114,88,45,76,123,87,25,23,
+				  200,122,150,90,92,87,177,244,
+				  201,6,12,60,8,2,5,67,7,87,
+				  250,230,99,3,100,90};
+
+							
+	size = sizeof(data) / sizeof(data[zero]);
+	
+	//Print original unsorted array
+	print_array(data, size);
+	
+	//Sort the data in decending order
+	sort_array(data, size);
+
+	// Print the sorted array
+        printf("Sorted Data:\n");
+
+	for (int i = zero; i < size; i++)
+	{
+		printf("%d ", data[i]);
+	}
+	
+	printf("\n");
+	                                                         
+
+	//Print Statistics
+	print_statistics(data, data, size);
+
+	return zero;
+}                             
--- stats.h
+++ stats.h
@@ -1,40 +1,51 @@
 /******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
+  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+  *
+  * Redistribution, modification or use of this software in source or binary
+  * forms is permitted as long as the files maintain this copyright. Users are 
+  * permitted to modify this and use it to learn about the field of embedded
+  * software. Alex Fosdick and the University of Colorado are not liable for any
+  * misuse of this material. 
+  *
+  *****************************************************************************/
 /**
- * @file Kulkarni_Coursera 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author Sandesh Kulkarni
- * @date 31-10-2023
- *
- */
-#ifndef __STATS_H__
-#define __STATS_H__
-
-/* Add Your Declarations and Function Comments here */ 
-
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+  * File : Kulkarni_Coursera  
+  * Brief : This programme aims to create a simple application, that 
+  * performs statistical analysis on a dataset. 
+  *
+  * 
+  * Author : Sandesh Kulkarni
+  * Date : 05-11-2023
+  *
+  */
 
 
-#endif /* __STATS_H__ */
+#include <stdio.h>
+#define zero 0
+#define one 1
+
+// Function to swap two elements in an array
+void swap(unsigned char *a, unsigned char *b);
+
+// Function to sort an array in descending order using the bubble sort algorithm
+void sort_array(unsigned char arr[], int size);
+
+//function to find the maximum value in the array
+unsigned char find_maximum(const unsigned char *data, int size); 
+
+// Function to find the minimum value in the array
+unsigned char find_minimum(const unsigned char *data, int size);
+
+// Function to find the mean of the array
+int find_mean(const unsigned char *data, int size);
+
+// Function to find the median of the array
+int find_median(unsigned char arr[], const unsigned char *data, int size); 
+
+// Print the statistics
+int print_statistics(unsigned char arr[], unsigned char *data, int size);
+  
+//Print Given array
+void print_array(const unsigned char *data, int size);
+                                                     
+                             
